a6a27beace8fcb791ac5b30c17a52c135f650b09
Robert Guttersohn <54447208+rguttersohn@users.noreply.github.com>
2024-10-20 16:30:31 -0400

    fixes the order of updated on results

--- app/Http/Controllers/UpdatedOn.php
+++ app/Http/Controllers/UpdatedOn.php
@@ -3,20 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\ScheduleRun;
-use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 
 class UpdatedOn extends Controller
 {
     public function getUpdatedOn(){
-
+        
         if(Cache::has('updated_on')){
             return response(Cache::get('updated_on'))
                 ->header('From-Cache', 'true');
         }
 
-        $last_update = ScheduleRun::select('completed_on')->where('success', true)->first();
+        $last_update = ScheduleRun::select('completed_on')->where('success', true)->orderByDesc('completed_on')->first();
         $last_update_formatted = ['updated_on' => Carbon::parse($last_update->completed_on)->format('F j, Y')];
 
         Cache::put('updated_on', $last_update_formatted);
